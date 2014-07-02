b3afe7b67547532a8866f779ba53e37779411904
Mihyaeru <mihyaeru21@gmail.com>
2014-07-03 01:25:27 +0900

    windowサイズ変更に対応 close #5

--- static/js/app.js
+++ static/js/app.js
@@ -108,6 +108,15 @@ function main() {
       add_mesh_to_scene(message.image_url, scene);
     });
   });
+
+  // windowのリサイズに対応
+  window.addEventListener('resize', function() {
+    var width  = window.innerWidth;
+    var height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix()
+  }, false);
 }
 
 main();
